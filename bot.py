115e4954cec17f702a7200f14ac3a32031dd389d
heroichydro <93748801+heroichydro@users.noreply.github.com>
2021-11-06 21:13:21 -0400

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,10 +1,28 @@
 import discord
 from discord.ext import commands
+import asyncio
+from tokens import disc_token
 
-client = commands.bot(command_prefix = '.')
+client = commands.Bot(command_prefix = '.')
+
+
+def check(message: discord.Message, member):
+    return isinstance(message.channel, discord.DMChannel) and message.author == member
 
 @client.event
 async def on_ready():
     print('Bot is ready.')
 
-client.run('OTA2NjMyNDIzNDA2NzE5MDU2.YYbdPg.9eeo7ZkATMYjm_Pr3pKWNiLoPM8')
+
+@client.command()   
+async def dm(ctx, member : discord.Member, *, content: str):
+    await member.send(content)
+            
+    try:
+        await member.send("How would you like to respond to your Secret Santa?")
+        reply = await client.wait_for("message", check = lambda message: isinstance(message.channel, discord.DMChannel) and message.author == member)
+        await ctx.author.send(reply.content)
+    except asyncio.TimeoutError:
+        pass
+
+client.run(disc_token)

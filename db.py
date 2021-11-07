b124be863bbfaadc0b7ab380014e5f5ef5a4f73a
Drin <adrien.bekker@gmail.com>
2021-11-07 09:56:54 -0500

    Update db.py

--- db.py
+++ db.py
@@ -1,16 +1,8 @@
 import pyrebase
 from tokens import config
-import random
 
 firebase = pyrebase.initialize_app(config)
 db = firebase.database()
-
-""" def store_user(msgID, userID):
-    db.child("msgIDs").child(msgID).push(userID)
-
-def retrieve_user(msgID):
-    userID = db.child("msgIDs").child(msgID).get()
-    return userID.val()[list(userID.val())[0]] """
 
 def get_pairs(server):
     pairs = db.child("server_pairs").child(server.id).get()
@@ -21,9 +13,3 @@ def upload_pairs(guild, pairs):
     # Upload data to cloud
 
     db.child("server_pairs").child(guild.id).push(pairs)        
-
-def access_santa(ctx):
-    pass
-
-def access_recipient(ctx):
-    pass
